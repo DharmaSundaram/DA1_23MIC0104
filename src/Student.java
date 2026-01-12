7af6ec9f539d620d3a08390ef17018aa4adaf8c3
DHARMA <DHARMA@LAPTOP-5UFLGI9G>
2026-01-12 08:34:53 +0530

    upload of java file

---
+++ src/Student.java
@@ -0,0 +1,7 @@
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("Student Name   : Dharma S");
+        System.out.println("Register Number: 23MIC0104");
+        System.out.println("Message        : Jenkins–GitHub Integration Successful");
+    }
+}
